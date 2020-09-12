654dd88b6ccb36e047936930f7fb633faee2c3fb
Lucas Tan <Ming@Lucas-Mac.local>
2020-09-12 17:18:31 +0800

    Update preliminary analysis

--- script/eda_preprocess.R
+++ script/eda_preprocess.R
@@ -1,6 +1,13 @@
-
+library(naniar)
+library(ggplot2)
+library(mice)
 
 store <- read.csv('csv/DA1920_store.csv')
 train <- read.csv('csv/DA1920_train.csv')
 test <- read.csv('csv/DA1920_test.csv')
+
+# Missing data in store
+gg_miss_var(store)
+
+# Density plot of missing data in store
 
